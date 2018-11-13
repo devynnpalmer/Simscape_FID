6ee928a548c8b1d796ec22d3e224920296830d61
Tyler-Kleinschmidt <43116612+Tyler-Kleinschmidt@users.noreply.github.com>
2018-11-12 19:44:12 -0600

    Update DrRPG.java

--- core/src/com/libgdx/game/DrRPG.java
+++ core/src/com/libgdx/game/DrRPG.java
@@ -1,33 +1,36 @@
 package com.libgdx.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class DrRPG extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.libgdx.game.screens.MainMenu;
+
+
+
+public class DrRPG extends Game {
+	
+	public static final int WIDTH = 1280;
+	public static final int HEIGHT = 720;
+	
+	public SpriteBatch batch;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+		this.setScreen(new MainMenu(this));
+		
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		
+	super.render();
 	}
 	
 	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void resize (int width, int height) {
+	super.resize(width, height);
 	}
 }
