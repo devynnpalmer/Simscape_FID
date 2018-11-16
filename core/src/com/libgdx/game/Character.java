750f69d1414bf1cf906eefc56b9db3932d221c02
Tim_Nguyen <ttn19@students.uwf.edu>
2018-11-16 10:15:48 -0600

    rename CharacterModel and reUpload Character

--- core/src/com/libgdx/game/Character.java
+++ core/src/com/libgdx/game/Character.java
@@ -1,48 +1,64 @@
-package com.mygdx.gameobject;
-
-import com.badlogic.gdx.math.Rectangle;
+package com.libgdx.game;
 
 public class Character {
+	private static String classStats = System.getProperty("user.dir") + "/SE2018MW_Group4/Character Class/CharacterClassSpecs.txt";
+	private static String classInfo = System.getProperty("user.dir") + "/SE2018MW_Group4/Character Class/CharacterClassInfo.txt";
+
+	private int currentMP;
+	private int currentXP;
+	private int currentHP;
 	
-	private Rectangle rectangle;
+	private int maxMP;
+	private int maxHP;
+	private int maxXP;
 	
-	public Character() {
-		rectangle = new Rectangle(0, 0, 30, 25);
+	private int level;
+	private String name;
+	
+	
+	private int normalDMG;
+	private int magicDMG;
+	private int rangeDMG;
+	
+	CharacterClass Classes;
+	private int classChoice; //0, professor 1, TA  2, Advisor
+	
+
+	/**
+	 * Constructor for character
+	 */
+	public Character()
+	{
+		this.currentHP = 0;
+		this.currentMP = 0;
+		this.currentXP = 0;
+		
+		this.level = 0;
+		this.maxHP = 0;
+		this.maxMP = 0;
+		this.maxXP = 0;
+		
+		this.normalDMG = 0;
+		this.magicDMG = 0;
+		this.rangeDMG = 0;
+		
 	}
 	
-	public Rectangle getRectangle() {
-		return rectangle;
+	/**
+	 * setClassChoice sets the choice the user select for class
+	 * @param classChoice
+	 */
+	public void setClassChoice(int classChoice)
+	{
+		this.classChoice = classChoice;
 	}
 	
-	public void onPressDOWN() {
-		rectangle.setPosition(rectangle.x, rectangle.y + 15);
+	
+	public void configClass()
+	{
+		this.Classes = new CharacterClass(classChoice);
+		Classes.readSpecs(classStats);
+		
 	}
 	
-	public void onPressUP() {
-		rectangle.setPosition(rectangle.x, rectangle.y - 15);
-	}
-	
-	public void onPressRIGHT() {
-		rectangle.setPosition(rectangle.x + 15, rectangle.y);
-	}
-	
-	public void onPressLEFT() {
-		rectangle.setPosition(rectangle.x - 15, rectangle.y);
-	}
-	
-	public void upperBoundX() {
-		rectangle.setPosition(720, rectangle.y);
-	}
-	
-	public void lowerBoundX() {
-		rectangle.setPosition(0, rectangle.y);
-	}
-	
-	public void upperBoundY() {
-		rectangle.setPosition(rectangle.x, 725);
-	}
-	
-	public void lowerBoundY() {
-		rectangle.setPosition(rectangle.x, 0);
-	}
 }
