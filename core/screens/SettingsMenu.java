315a8d8f52457141a5af9a8581e96ad3a6e6ebf9
Tyler-Kleinschmidt <43116612+Tyler-Kleinschmidt@users.noreply.github.com>
2018-11-18 14:33:13 -0600

    Add files via upload

---
+++ core/screens/SettingsMenu.java
@@ -0,0 +1,54 @@
+package com.libgdx.game.screens;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
+import com.libgdx.game.DrRPG;
+
+public class SettingsMenu implements Screen{
+
+	final DrRPG game;
+
+	Texture settingsMenu;
+	
+	public SettingsMenu(DrRPG game) {
+		this.game = game;
+		
+	}
+	
+	
+	@Override
+	public void show() {
+		
+	}
+
+	@Override
+	public void render(float delta) {
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		
+	}
+
+	@Override
+	public void pause() {
+		
+	}
+
+	@Override
+	public void resume() {
+		
+	}
+
+	@Override
+	public void hide() {
+		
+	}
+
+	@Override
+	public void dispose() {
+		
+	}
+
+}
